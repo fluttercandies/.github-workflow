cba2f0d24111e1ff64a21c5196b1c8eacc7a934c
CaiJingLong <cjl_spy@163.com>
2023-11-02 08:30:46 +0800

    ci: Upgrade make profile readme
    Signed-off-by: CaiJingLong <cjl_spy@163.com>

---
+++ make-profile-readme.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from os.path import join
+
+template_file_name = 'README.template.md'
+repo_path = '/tmp/.github'
+
+with open(join(repo_path, template_file_name), 'r') as f:
+    template = f.read()
+
+with open('/tmp/TABLE.md', 'r') as f:
+    table = f.read()
+
+
+dest_text = template.replace('<!-- REPO-LIST -->', table)
+
+
+with open(join(repo_path, 'profile', 'README.md'), 'w') as f:
+    f.write(dest_text)
